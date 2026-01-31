3e6975639e684b52690fa1ac32603605a61059df
Barton Willis <willisb@unk.edu>
2026-01-31 04:53:26 -0600

    name change
    rename  define-converter  to define-function-converter

--- function_convert.lisp
+++ function_convert.lisp
@@ -54,11 +54,11 @@ Each entry has the form:
     (fapply 'mlist (nreverse results))))
 
 
-(defmacro define-converter ((from to) lambda-list &body body)
+(defmacro define-function-converter ((from to) lambda-list &body body)
   "Define a converter from FROM to TO, automatically naming the function
 CONVERTER-FROM-TO, installing it in the converter registry, and returning
 the function symbol."
-  (let* ((fname (intern (format nil "CONVERTER-~A-~A"
+  (let* ((fname (intern (format nil "FUNCTION-CONVERTER-~A-~A"
                                 from
                                 to)
                         :maxima)))
@@ -162,38 +162,38 @@ the function symbol."
 
 ;; Debugging Hint: If you define a converter that doesn't trigger correctly, try
 ;; tracing lookup-converter and look at the output of list_converters.
-;; In define-converter, don't quote the source and target functions.
-(define-converter (%sinc %sin) (x)
+;; In define-function-converter, don't quote the source and target functions.
+(define-function-converter (%sinc %sin) (x)
   "Convert sinc(x) into sin(x)/x."
   (let ((z (car x)))
     (div (ftake '%sin z) z)))
 
-(define-converter (%sinc %gamma) (x)
+(define-function-converter (%sinc %gamma) (x)
   "Convert sinc(x) into 1/((gamma(1+x/%pi))*gamma(1-x/%pi))."
   (let ((z (div (car x) '$%pi)))
     (div 1 (mul (ftake '%gamma (add 1 z)) (ftake '%gamma (sub 1 z))))))
 
-(define-converter (%sin %sinc) (x)
+(define-function-converter (%sin %sinc) (x)
   "Convert sin(x) into x*sinc(x)."
   (let ((z (car x)))
     (mul z (ftake '%sinc z))))
 
-(define-converter (mfactorial %gamma) (x)
+(define-function-converter (mfactorial %gamma) (x)
  "Convert x! into gamma(1+x)."
   (let ((z (car x))) (ftake '%gamma (add 1 z))))
 
-(define-converter (%csc %sin) (x)
+(define-function-converter (%csc %sin) (x)
   "Convert csc(x) into 1/sin(x)."
     (let ((z (car x))) (div 1 (ftake '%sin z))))
 
 ;; tan → sin/cos
-(define-converter (%tan %sin) (x)
+(define-function-converter (%tan %sin) (x)
 "Convert tan(x) into sin(x)/cos(x)."
   (let ((z (car x)))
     (div (ftake '%sin z)
          (ftake '%cos z))))
 
-(define-converter (%sin %exp) (x)
+(define-function-converter (%sin %exp) (x)
   "Convert sin(x) to exponential form."
   (let ((z (car x)))
     (div
@@ -201,7 +201,7 @@ the function symbol."
            (ftake '%exp (mul (neg '$%i) z)))
       (mul 2 '$%i))))
 
-(define-converter (%cos %exp) (x)
+(define-function-converter (%cos %exp) (x)
 "Convert cos(x) to exponential form."
   (let ((z (car x)))
     (div
@@ -209,7 +209,7 @@ the function symbol."
            (ftake '%exp (mul (neg '$%i) z)))
       2)))
 
-(define-converter (%sinh %exp) (x)
+(define-function-converter (%sinh %exp) (x)
 "Convert sinh(x) to exponential form."
   (let ((z (car x)))
     (div
@@ -217,7 +217,7 @@ the function symbol."
            (ftake '%exp (neg z)))
       2)))
 
-(define-converter (%cosh %exp) (x)
+(define-function-converter (%cosh %exp) (x)
 "Convert cosh(x) to exponential form."
   (let ((z (car x)))
     (div
@@ -226,26 +226,26 @@ the function symbol."
       2)))
 
 ;; tanh → sinh/cosh
-(define-converter (%tanh %sinh) (x)
+(define-function-converter (%tanh %sinh) (x)
 "Convert tanh(x) to sinh(x)/cosh(x)."
   (let ((z (car x)))
     (div (ftake '%sinh z)
          (ftake '%cosh z))))
 
 ;; double_factorial → gamma
-(define-converter (%genfact %gamma) (x)
+(define-function-converter (%genfact %gamma) (x)
 "Convert x!! to gamma form."
   (let ((a (car x)) (b (cadr x)) (c (caddr x))) ($makegamma (ftake '%genfact a b c))))
 
 ;; log10(x) → log(x)/log(10)
-(define-converter ($log10 %log) (x)
+(define-function-converter ($log10 %log) (x)
   "Convert log10(x) into log(x)/log(10)."
   (let ((z (car x)))
     (div (ftake '%log z)  (ftake '%log 10))))
 
 ;; I could do logarc transformations, but for now, let's not.
 
-(define-converter (%binomial mfactorial) (x)
+(define-function-converter (%binomial mfactorial) (x)
 "Convert binomial(n,k) to factorial form."
   (let ((n (car x))
         (k (cadr x)))
@@ -254,17 +254,17 @@ the function symbol."
               (ftake 'mfactorial (sub n k))))))
 
  ;;"!" => product does n! => product(%g23,%g23,1,n)
- (define-converter (mfactorial $product) (x)
+ (define-function-converter (mfactorial $product) (x)
   "Convert n! to product(g,g,1,n)."
   (let ((z (car x)) (g ($gensym)))
     (ftake '%product g g 1 z)))
 
-(define-converter (%atan %log) (x)
+(define-function-converter (%atan %log) (x)
   "Convert tan(x) to logarc form."
   (let ((z (car x)))
     ($logarc (ftake '%atan z))))
 
- (define-converter (%gamma_incomplete %expand) (x)
+ (define-function-converter (%gamma_incomplete %expand) (x)
   (let ((a (car x)) (z (cadr x)) ($gamma_expand t))
     (ftake '%gamma_incomplete a z)))
    
@@ -273,7 +273,7 @@ the function symbol."
 ;; function must be mexpt, not a trigonometric function.
 
 ;; All the business about the gensym is to prevent non-trig functions from expanding (well, that's my claim).
-(define-converter (mexpt $trigreduce) (x)
+(define-function-converter (mexpt $trigreduce) (x)
  "Convert integer powers of trig to a Fourier sum"
   (let ((z (car x)) (n (cadr x)))
     (cond ((and (consp z) (consp (car z)) (trigp (caar z)) (integerp n))
@@ -286,62 +286,62 @@ the function symbol."
 ;; erf-like functions
 
 
-(define-converter (%erfi %erf) (x)
+(define-function-converter (%erfi %erf) (x)
   "Convert erfi(x) into -i * erf(i*x)."
   (let ((z (car x)))
     (mul -1 '$%i (ftake '%erf (mul '$%i z)))))
 
 
-(define-converter (%erf %erfi) (x)
+(define-function-converter (%erf %erfi) (x)
   "Convert erf(x) into i * erfi(-i*x)."
   (let ((z (car x)))
     (mul '$%i (ftake '%erfi (mul -1 '$%i z)))))
 
-(define-converter (%erf %erfc) (x)
+(define-function-converter (%erf %erfc) (x)
   "Convert erf(x) into 1 - erfc(x)."
   (let ((z (car x)))
     (sub 1 (ftake '%erfc z))))
 
-(define-converter (%erfc %erf) (x)
+(define-function-converter (%erfc %erf) (x)
   "Convert erfc(x) into 1 - erf(x)."
   (let ((z (car x)))
     (sub 1 (ftake '%erf z))))
 
-(define-converter (%erf $integral) (x)
+(define-function-converter (%erf $integral) (x)
   "Convert erfc(x) into an integral representation"
   (let ((z (car x))
         (s (gentemp "$X" :maxima)))
          (mul (div 2 (ftake 'mexpt '$%pi (div 1 2)))
            (ftake '%integrate  (ftake 'mexpt '$%e (mul -1 s s)) s  0 z))))
 
-(define-converter (%erf %hypergeometric) (x)
+(define-function-converter (%erf %hypergeometric) (x)
   "Convert erf(x) into (2*x/sqrt(pi))*hypergeometric([1/2],[3/2],-x^2)."
   (let ((z (car x))) 
     (let (($hypergeometric_representation t)) (ftake '%erf z))))
   
 ;; abs
-(define-converter (mabs %signum) (x)
+(define-function-converter (mabs %signum) (x)
   "Convert abs(x) into x*signum(x)."
   (let ((z (car x)))
     (mul z (ftake '%signum z))))
 
-(define-converter (mabs %sqrt) (x)
+(define-function-converter (mabs %sqrt) (x)
   "Convert abs(x) into sqrt(x^2). When radexpand is true, this is simplified back to abs(x)"
   (let ((z (car x)))
     (ftake 'mexpt (mul z (ftake '$conjugate z)) (div 1 2))))
 
-(define-converter (%signum %hstep) (x)
+(define-function-converter (%signum %hstep) (x)
   "Convert signum(x) into 2 hstep(x) - 1."
   (let ((z (car x)))
     (sub (mul 2 (ftake '%hstep z)) 1)))
 
-(define-converter (%hstep %signum) (x)
+(define-function-converter (%hstep %signum) (x)
   "Convert hstep(x) into (1+signum(x))/2."
   (let ((z (car x)))
     (div (add 1 (ftake '%signum z)) 2)))
 
 ;; disapointing to have to do this twice ...
-(define-converter (%sin %sin) (x)
+(define-function-converter (%sin %sin) (x)
   (let ((z (car x)))
     (cond
       (($polynomialp z (ftake 'mlist '$%pi)
@@ -352,7 +352,7 @@ the function symbol."
          (ftake '%sin (add w (reduce-angle-mod-2pi (mul '$%pi n))))))
       (t  (ftake '%sin z)))))
     
-(define-converter (%cos %cos) (x)
+(define-function-converter (%cos %cos) (x)
   (let ((z (car x)))
     (cond
       (($polynomialp z (ftake 'mlist '$%pi)
