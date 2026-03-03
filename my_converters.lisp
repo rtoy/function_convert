033e41f195ca5f9ac2a2a3a7eeea882123359e65
Barton Willis <willisb@unk.edu>
2026-03-03 06:07:07 -0600

    append glossary to manual
    o append glossary to manual
    o some tweaks to the manual

--- my_converters.lisp
+++ my_converters.lisp
@@ -4,3 +4,10 @@
 (defmvar *box-counter* 0)    
 (define-function-converter ((:algebraic $boxed) ($algebraic $boxed)) (op x)
   (ftake 'mlabox (fapply op x) (incf *box-counter* 1)))
+
+
+;; This maybe demonstrates that converters should be based on identities--when they are
+;; not, the results are predicable, but rubbish.
+(define-function-converter ((:algebraic $integrate) ($algebraic $integrate)) (op x)
+  (let ((e (fapply op x)))
+    ($integrate e '$x)))
