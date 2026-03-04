d73c0e6733ac3a28f83e1700ac4acd83a1f1eecc
Barton Willis <willisb@unk.edu>
2026-03-04 07:22:24 -0600

    various tweaks  for manual
    o citations for error functions
    o cross references for some sections
    o show how to define the algebraic = boxed converter
    o make capitalization of headings
    o new section on why base converters on identities
    o alphabetize glossary
    o change macro \funconvert
    o various tweaks

--- my_converters.lisp
+++ my_converters.lisp
@@ -1,9 +1,9 @@
-
-;; This converter shows the traversal order--it puts subexpressions whose head operator
-;; is +,*, or ^ in a labeled box numbered consecutively.
-(defmvar *box-counter* 0)    
+(defmvar $box_counter 0)    
 (define-function-converter ((:algebraic $boxed) ($algebraic $boxed)) (op x)
-  (ftake 'mlabox (fapply op x) (incf *box-counter* 1)))
+ "This converter puts subexpressions whose head operator is +,*, or ^ in a labeled box numbered consecutively.
+  Other than showing the postorder application of converters, it serves no purpose. The box counter is
+ stored in a special variable `box_counter'.  "
+  (ftake 'mlabox (fapply op x) (incf $box_counter 1)))
 
 
 ;; This maybe demonstrates that converters should be based on identities--when they are
